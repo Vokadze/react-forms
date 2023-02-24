027d62a2d92c5dfa0ceae0e51bfb74f2c2f21746
Esdakov Sergey <esdakov@mail.ru>
2023-02-24 17:34:58 +0300

    lesson 9 path 1. 1. Form creation

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MyForm from './components/myFomr'
+
+function App() {
+  return <MyForm />
+}
+
+export default App
---
+++ src/components/myFomr.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyForm = () => {
+   const handleSubmit = (e) => {
+
+   }
+   return (
+      <h1>MyForms</h1>
+   )
+}
+
+export default MyForm;

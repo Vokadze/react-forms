af90df42fb3311f483c3b5fd34a70140d84e788c
Esdakov Sergey <esdakov@mail.ru>
2023-02-26 14:36:32 +0300

    lesson 9 change disabled button

--- src/components/myForm.jsx
+++ src/components/myForm.jsx
@@ -4,71 +4,81 @@ import TextField from "./textField";
 import { validationSchema } from "./validationSchema";
 
 const MyForm = () => {
-    const [values, setValues] = useState({
-        email: "",
-        link: "",
-        description: ""
-    });
-    const [errors, setErrors] = useState({});
+   const [values, setValues] = useState({
+      email: "",
+      link: "",
+      description: ""
+   });
+   const [errors, setErrors] = useState({});
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+   /**
+    * Object.keys() вернёт массив ключей объекта с ошибками.
+    * Если длина этого массива равна 0, то ошибок нет
+    */
+   const isValid = Object.keys(errors).length === 0;
 
-    const handleChange = (e) => {
-        const { value, name } = e.target;
-        setValues((prev) => ({
-            ...prev,
-            [name]: value
-        }));
-    };
+   useEffect(() => {
+      const errors = validate(values, validationSchema);
+      setErrors(errors);
+   }, [values]);
 
-    //  Диструкторизируем для удобства
-    const { email, link, description } = values;
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      if (isValid) {
+         // отправляем если только валидно
+         console.log("Отправлено!");
+      }
+   };
 
-    useEffect(() => {
-        const errors = validate(values, validationSchema);
-        setErrors(errors);
-    }, [values]);
+   const handleChange = (e) => {
+      const { value, name } = e.target;
+      setValues((prev) => ({
+         ...prev,
+         [name]: value
+      }));
+   };
 
-    return (
-        <div>
-            <h2>Отчёт об ошибке</h2>
-            {/* Для удобства просмотра */}
-            <div>
-                <pre>{JSON.stringify(errors, null, 2)}</pre>
-            </div>
+   //  Диструкторизируем для удобства
+   const { email, link, description } = values;
 
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    id="email"
-                    name="email"
-                    label="Email"
-                    value={email}
-                    onChange={handleChange}
-                    error={errors.email}
-                />
-                <TextField
-                    id="link"
-                    name="link"
-                    label="Ссылка на страницу с ошибкой"
-                    value={link}
-                    onChange={handleChange}
-                    error={errors.link}
-                />
-                <TextField
-                    id="description"
-                    name="description"
-                    label="Описание"
-                    value={description}
-                    onChange={handleChange}
-                    error={errors.description}
-                />
+   return (
+      <div>
+         <h2>Отчёт об ошибке</h2>
+         {/* Для удобства просмотра */}
+         <div>
+            <pre>{JSON.stringify(errors, null, 2)}</pre>
+         </div>
 
-                <button type="submit">Отправить</button>
-            </form>
-        </div>
-    );
+         <form onSubmit={handleSubmit}>
+            <TextField
+               id="email"
+               name="email"
+               label="Email"
+               value={email}
+               onChange={handleChange}
+               error={errors.email}
+            />
+            <TextField
+               id="link"
+               name="link"
+               label="Ссылка на страницу с ошибкой"
+               value={link}
+               onChange={handleChange}
+               error={errors.link}
+            />
+            <TextField
+               id="description"
+               name="description"
+               label="Описание"
+               value={description}
+               onChange={handleChange}
+               error={errors.description}
+            />
+
+            <button disabled={!isValid} type="submit">Отправить</button>
+         </form>
+      </div>
+   );
 };
 
 export default MyForm;
